fbf5792db0b87d258ad2fb6d866a9b7c8262279e
selengr <reza1997karbakhsh@gmail.com>
2025-03-25 17:53:33 +0330

    create metadataTranslations

--- config-global.ts
+++ config-global.ts
@@ -1,8 +1,13 @@
 
-export const APP_NAME = "آوینــــــــا";
-export const APP_DEFAULT_TITLE = "آوینــــــــا";
-export const APP_TITLE_TEMPLATE = "%s - آوینــــــــا";
-export const APP_DESCRIPTION = "آوینــــــــا | ما یک تیم خلاق و قدرتمند هستیم";
+export const APP_NAME_EN = "smart Insurance portal";
+export const APP_DEFAULT_TITLE_EN = "smart Insurance portal";
+export const APP_TITLE_TEMPLATE_EN = "%s - smart Insurance portal";
+export const APP_DESCRIPTION_EN = "smart Insurance portal | this is a Smart Insurance Application Portal, where users can apply for different types of insurance (Health, Home, Car, Life, etc.) through a dynamic form.";
+
+export const APP_NAME_FA = "پورتال  هوشمند بیمه";
+export const APP_DEFAULT_TITLE_FA = "پورتال  هوشمند بیمه";
+export const APP_TITLE_TEMPLATE_FA = "%s - پورتال  هوشمند بیمه";
+export const APP_DESCRIPTION_FA = "پورتال  هوشمند بیمه | این یک پورتال کاربردی بیمه هوشمند است که در آن کاربران می توانند از طریق یک فرم پویا برای انواع مختلف بیمه (سلامت، خانه، خودرو، زندگی و غیره) درخواست دهند.";
 
 
 // API
