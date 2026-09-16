aafacd2e29e09bfff4e41fa57136632495c6aa71
selengr <rezaa997karbakhsh@gmail.com>
2026-09-17 00:27:43 +0330

    purchased list works offline too

--- src/services/api/purchased-insurances.tsx
+++ src/services/api/purchased-insurances.tsx
@@ -1,9 +1,19 @@
 import httpService from "../http-service";
 import { ITabelData } from "@/types/purchased-insurances";
+import {
+  isMockApiEnabled,
+  mockPurchasedColumns,
+  mockPurchasedRows,
+} from "@/mocks/fixtures";
 
-
-//------------------------------------------------------------------------------------------------------------
 export const purchasedInsurancesApi = async (): Promise<ITabelData> => {
-     const {data} = await httpService.get(`/api/insurance/forms/submissions`)
-     return data
-}
+  if (isMockApiEnabled()) {
+    return {
+      columns: mockPurchasedColumns,
+      data: mockPurchasedRows,
+    };
+  }
+
+  const { data } = await httpService.get(`/api/insurance/forms/submissions`);
+  return data;
+};
