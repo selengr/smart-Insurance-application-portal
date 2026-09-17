b036fa779422c8a3c5307177b94f031c0c50089d
selengr <reza1997karbakhsh@gmail.com>
2026-09-18 02:47:23 +0330

    middleware: drop ts-expect-error, skip static files

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,21 +10,18 @@ function getLocale(request: NextRequest): string | undefined {
   const negotiatorHeaders: Record<string, string> = {}
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
 
-  // @ts-expect-error locales are readonly
-  const locales: string[] = i18n.locales
+  const locales = [...i18n.locales]
   const languages = new Negotiator({ headers: negotiatorHeaders }).languages()
 
-  const locale = matchLocale(languages, locales, i18n.defaultLocale)
-  return locale
+  return matchLocale(languages, locales, i18n.defaultLocale)
 }
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
   const pathnameIsMissingLocale = i18n.locales.every(
-    locale => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
   )
 
-  // Redirect if there is no locale
   if (pathnameIsMissingLocale) {
     const locale = getLocale(request)
     return NextResponse.redirect(
@@ -34,9 +31,10 @@ export function middleware(request: NextRequest) {
       )
     )
   }
+
+  return NextResponse.next()
 }
 
 export const config = {
-  // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|fonts|icons|.*\\..*).*)'],
 }
